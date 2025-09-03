d082cc9f3451a05675ad96e9b5aab5e3a439cfcf
jessid <jessid.velez@protonmail.com>
2025-09-04 00:00:40 +0200

    added scripts directory and simple install commands

---
+++ scripts/install_commonly_used.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y htop
+sudo apt install -y terminator
+sudo apt install -y flameshot
+sudo apt install -y build-essential
+sudo apt install -y git
+sudo apt install -y openssh-server
+sudo apt install -y curl
+sudo apt install -y cmake-curses-gui
