7c81d6b6fb7b24cf0f78c03915fe0cb758085cf0
jessid <jessid.velez@protonmail.com>
2025-09-09 13:33:49 +0200

    improved debug

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -5,67 +5,132 @@ return {
     dependencies = {
       "rcarriga/nvim-dap-ui",
       "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim", 
     },
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      require("dapui").setup()
-      -- require(dapui).setup()
+      local dap = require "dap"
+      local dapui = require "dapui"
 
-      -- Detecta lldb-dap (nuevo) o lldb-vscode (antiguo)
-      local lldb_cmd = vim.fn.exepath("lldb-vscode-14")
-      if lldb_cmd == "" then
-        lldb_cmd = vim.fn.exepath("lldb")
+      local function find_codelldb()
+        -- Ruta típica de Mason en Linux:
+        local mason_adapter = vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/adapter/codelldb"
+        if vim.loop.fs_stat(mason_adapter) then
+          return mason_adapter
+        end
+        -- Si no está en Mason, intenta en PATH
+        local in_path = vim.fn.exepath "codelldb"
+        if in_path ~= "" then
+          return in_path
+        end
+        return nil
       end
-      if lldb_cmd == "" then
-        vim.notify("[dap] No encuentro lldb-dap/lldb-vscode en PATH. Instala 'lldb'.", vim.log.levels.ERROR)
+
+      local codelldb_cmd = find_codelldb()
+      if not codelldb_cmd then
+        vim.notify(
+          "No encuentro el adapter 'codelldb'. Abre :Mason y instala 'codelldb', o añade codelldb al PATH.",
+          vim.log.levels.ERROR
+        )
         return
       end
 
-      -- Adapter LLDB
-      dap.adapters.lldb = {
-        type = "executable",
-        command = lldb_cmd,
-        name = "lldb",
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = codelldb_cmd,
+          args = { "--port", "${port}" },
+        },
       }
 
-      -- Config C/C++
       dap.configurations.cpp = {
         {
-          name = "LLDB: Launch (build/app)",
-          type = "lldb",
+          name = "Run (pick exe)",
+          type = "codelldb",
           request = "launch",
           program = function()
-            local default = vim.fn.getcwd() .. "/build/app"
-            if vim.fn.executable(default) == 1 or vim.fn.filereadable(default) == 1 then
-              return default
-            end
-            return vim.fn.input("Ruta al ejecutable: ", default, "file")
+            return vim.fn.input("Ruta al ejecutable: ", vim.fn.getcwd() .. "/build/", "file")
           end,
           cwd = "${workspaceFolder}",
           stopOnEntry = false,
           args = {},
-          runInTerminal = false, -- pon true si tu app necesita TTY (stdin)
+        },
+
+        {
+          name = "Run (pick exe + args)",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Ruta al ejecutable: ", vim.fn.getcwd() .. "/build/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = function()
+            local line = vim.fn.input "Args: "
+            return (line == "" and {}) or vim.fn.split(line, " ")
+          end,
+        },
+
+        {
+          name = "Run build/app (no args)",
+          type = "codelldb",
+          request = "launch",
+          program = "${workspaceFolder}/build/",
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = {},
+        },
+
+        {
+          name = "Run build/app (TTY + args)",
+          type = "codelldb",
+          request = "launch",
+          program = "${workspaceFolder}/build/",
+          cwd = "${workspaceFolder}",
+          runInTerminal = true,
+          args = function()
+            local line = vim.fn.input "Args: "
+            return (line == "" and {}) or vim.fn.split(line, " ")
+          end,
+        },
+
+        {
+          name = "Attach to process",
+          type = "codelldb",
+
+          request = "attach",
+          pid = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
         },
       }
+
       dap.configurations.c = dap.configurations.cpp
 
-      -- UI
       dapui.setup()
-      dap.listeners.before.attach.dapui_config = function() dapui.open() end
-      dap.listeners.before.launch.dapui_config = function() dapui.open() end
-      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
-      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-      -- Teclas
       local map = vim.keymap.set
       map("n", "<F5>", dap.continue, { desc = "DAP Continue" })
-      map("n", "<F10>", dap.step_over, { desc = "DAP Step Over" })
-      map("n", "<F11>", dap.step_into, { desc = "DAP Step Into" })
-      map("n", "<F12>", dap.step_out, { desc = "DAP Step Out" })
+      map("n", "<F6>", dap.step_over, { desc = "DAP Step Over" })
+      map("n", "<F7>", dap.step_into, { desc = "DAP Step Into" })
+      map("n", "<F8>", dap.step_out, { desc = "DAP Step Out" })
       map("n", "<leader>tb", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
 
-      -- Señales
+      map("n", "K", function()
+        dapui.eval()
+      end, { desc = "DAP eval" })
+
       vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError" })
       vim.fn.sign_define("DapStopped", { text = "▶", texthl = "DiagnosticInfo" })
     end,
