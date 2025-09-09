b504c547c905a55962fdf5efaf1550ae26093c7a
jessid <jessid.velez@protonmail.com>
2025-09-09 11:14:13 +0200

    added configuration for debugging using lldb-vscode-14

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,83 +1,73 @@
 return {
-  -- Plugin base
   {
     "mfussenegger/nvim-dap",
-    lazy = true,
-  },
-
-  -- UI para DAP (requiere nvim-nio ahora)
-  {
-    "rcarriga/nvim-dap-ui",
+    lazy = false,
     dependencies = {
-      "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio", -- <--- importante!
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
     },
     config = function()
-      local dap, dapui = require "dap", require "dapui"
-      dapui.setup()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      require("dapui").setup()
+      -- require(dapui).setup()
 
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
+      -- Detecta lldb-dap (nuevo) o lldb-vscode (antiguo)
+      local lldb_cmd = vim.fn.exepath("lldb-vscode-14")
+      if lldb_cmd == "" then
+        lldb_cmd = vim.fn.exepath("lldb")
       end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
+      if lldb_cmd == "" then
+        vim.notify("[dap] No encuentro lldb-dap/lldb-vscode en PATH. Instala 'lldb'.", vim.log.levels.ERROR)
+        return
       end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
 
-  -- Adaptador para GDB (como antes)
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require "dap"
-      dap.adapters.cppdbg = {
-        id = "cppdbg",
+      -- Adapter LLDB
+      dap.adapters.lldb = {
         type = "executable",
-        command = vim.fn.stdpath "data" .. "/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+        command = lldb_cmd,
+        name = "lldb",
       }
 
+      -- Config C/C++
       dap.configurations.cpp = {
         {
-          name = "Launch file",
-          type = "cppdbg",
+          name = "LLDB: Launch (build/app)",
+          type = "lldb",
           request = "launch",
           program = function()
-            return vim.fn.input("Ruta al ejecutable: ", vim.fn.getcwd() .. "/", "file")
+            local default = vim.fn.getcwd() .. "/build/app"
+            if vim.fn.executable(default) == 1 or vim.fn.filereadable(default) == 1 then
+              return default
+            end
+            return vim.fn.input("Ruta al ejecutable: ", default, "file")
           end,
-          --cwd = "${workspaceFolder}",
-          cwd = vim.fn.expand "~/ros2_ws/build/loris/", -- ← esta línea fija la raíz del proceso
-          stopAtEntry = true,
-          setupCommands = {
-            {
-              text = "-enable-pretty-printing",
-              description = "habilita pretty-printing",
-              ignoreFailures = false,
-            },
-          },
-        },
-        {
-          name = "Attach to process",
-          type = "cppdbg",
-          request = "attach",
-          processId = require("dap.utils").pick_process,
-          cwd = vim.fn.expand ("~/ros2_ws/build/loris/"),
-          program = function()
-            return vim.fn.input("Ruta al binario: ", "/home/jessid/ros2_ws/build/loris/" .. "/", "file")
-          end,
-          setupCommands = {
-            {
-              text = "-enable-pretty-printing",
-              description = "habilita pretty-printing",
-              ignoreFailures = false,
-            },
-          },
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = {},
+          runInTerminal = false, -- pon true si tu app necesita TTY (stdin)
         },
       }
-
       dap.configurations.c = dap.configurations.cpp
+
+      -- UI
+      dapui.setup()
+      dap.listeners.before.attach.dapui_config = function() dapui.open() end
+      dap.listeners.before.launch.dapui_config = function() dapui.open() end
+      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+
+      -- Teclas
+      local map = vim.keymap.set
+      map("n", "<F5>", dap.continue, { desc = "DAP Continue" })
+      map("n", "<F10>", dap.step_over, { desc = "DAP Step Over" })
+      map("n", "<F11>", dap.step_into, { desc = "DAP Step Into" })
+      map("n", "<F12>", dap.step_out, { desc = "DAP Step Out" })
+      map("n", "<leader>tb", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
+
+      -- Señales
+      vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError" })
+      vim.fn.sign_define("DapStopped", { text = "▶", texthl = "DiagnosticInfo" })
     end,
   },
 }
